1d4ee4a0e3ff27cdb227fef66df1509deb07597b
stephenbowyer <github@ste.widearea.org>
2024-10-25 17:14:59 +0100

    Add ability to scroll views

--- exhibitly/components/CollectionList.tsx
+++ exhibitly/components/CollectionList.tsx
@@ -1,36 +1,43 @@
 import React, {FC, useState, useEffect} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, ScrollView} from 'react-native';
 import { fetchItems, fetchAllItems, shuffleItems } from '../utils';
 import ItemCard from "./ItemCard";
 
 const CollectionList:FC = ()  => {
     const [allItems, setAllItems] = useState(new Array());
     useEffect(() => {
-        fetchAllItems().then(items => {
-                shuffleItems(items);
-                if (items[0].hasOwnProperty('id'))
-                    setAllItems(items);
-                else
-                    setAllItems(items.flat());
-        }).catch((result) => {
-            console.log("ERROR", result); //[AxiosError: Request failed with status code 404]
-        });
+        if (allItems.length === 0){
+            fetchAllItems().then(items => {
+                    shuffleItems(items);
+                    if (items[0].hasOwnProperty('id'))
+                        setAllItems(items);
+                    else
+                        setAllItems(items.flat());
+            }).catch((result) => {
+                console.log("ERROR", result); //[AxiosError: Request failed with status code 404]
+            });
+        }
     }, []);
     return (
-        <View style={styles.collections}>
-            {allItems?.map((item) => <ItemCard itemObj={item} key={item.id}/>)}
-        </View>
+        <ScrollView style={styles.scrollView}>
+            <View style={styles.collections}>
+            {allItems?.map((item) => <ItemCard style={styles.card} itemObj={item} key={item.id} />)}
+            </View>
+        </ScrollView>
     )
 }
 const styles = StyleSheet.create({
     collections: {
       paddingVertical: 20,
-      borderBottomWidth: 1,
       flex: 1,
       alignItems: 'center',
       flexDirection: 'row',
       flexWrap: 'wrap',
       justifyContent: 'center',
+    },
+    scrollView: {
+    },
+    card: {
     },
 });
 export default CollectionList;
